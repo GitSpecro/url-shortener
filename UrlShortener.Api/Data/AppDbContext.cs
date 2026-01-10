f8103950aa5b81267cddf5b7e247935752bd55f2
GitSpecro <acvlok01@gmail.com>
2026-01-10 13:29:35 +0200

    Add AppDbContext

---
+++ UrlShortener.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Api.Models;
+
+namespace UrlShortener.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
