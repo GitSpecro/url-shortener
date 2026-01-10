c5bd8d0c6aa756e8009e35b49158d43568dea774
GitSpecro <acvlok01@gmail.com>
2026-01-10 12:14:29 +0200

    Add User and ShortUrl models

---
+++ UrlShortener.Api/Models/ShortUrl.cs
@@ -0,0 +1,12 @@
+﻿namespace UrlShortener.Api.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortCode { get; set; } = string.Empty;
+        public int ClickCount { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ UrlShortener.Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UrlShortener.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public decimal WalletBalance { get; set; }
+        public int SharePercentage { get; set; } = 10;
+    }
+}
